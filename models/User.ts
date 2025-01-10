1d4a890295edd58e57ecdce7399906247f8890ea
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-10 21:48:39 +0800

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -1,13 +1,5 @@
-import mongoose, { Document, Types } from 'mongoose';
-
-export interface UserType extends Document {
-  _id: Types.ObjectId;
-  clerkId: string;
-  username: string;
-  profileImage?: string;
-  following: Types.ObjectId[];
-  followers: Types.ObjectId[];
-}
+// models/User.ts
+import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   clerkId: { type: String, required: true, unique: true },
