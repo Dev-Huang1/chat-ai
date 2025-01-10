820af48a8e9e12ef8d833f6325cd22ee47b7859b
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-10 22:05:12 +0800

    Update PostType.ts

--- models/PostType.ts
+++ models/PostType.ts
@@ -1,10 +1,21 @@
 import mongoose from 'mongoose';
 
-export interface PostType {
-  _id: mongoose.Types.ObjectId;
-  content: string;
-  image?: string;
+interface PostType extends mongoose.Document {
   likes: mongoose.Types.ObjectId[];
-  comments: { content: string; author: mongoose.Types.ObjectId }[];
-  author: { username: string };
+  content: string;
+  author: mongoose.Types.ObjectId;
+  comments: { content: string, author: mongoose.Types.ObjectId }[];
+  image?: string;
 }
+
+const PostSchema = new mongoose.Schema({
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  comments: [{ content: String, author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } }],
+  image: String,
+});
+
+const Post = mongoose.models.Post || mongoose.model('Post', PostSchema);
+
+export default Post;
