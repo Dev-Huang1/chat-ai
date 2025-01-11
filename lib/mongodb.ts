056c259adc4d7f4090f3a41d2f51bca339f9c94e
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-11 13:31:41 +0800

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,7 +7,11 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options = {}
 
-let client
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined
+}
+
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
 if (process.env.NODE_ENV === 'development') {
@@ -24,5 +28,7 @@ if (process.env.NODE_ENV === 'development') {
   clientPromise = client.connect()
 }
 
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
 export default clientPromise
 
