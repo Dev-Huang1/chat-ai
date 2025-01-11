dc56d48895abe485b398d6b0d3b034f550611add
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-11 14:09:25 +0800

    Update page.tsx

--- pages/page.tsx
+++ pages/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-background flex flex-col items-center justify-center p-4">
       <div className="w-full max-w-2xl mx-auto mb-4 flex justify-between items-center">
-        <UserButton afterSignOutUrl="/" />
         <ThemeToggle />
       </div>
       <SignedIn>
