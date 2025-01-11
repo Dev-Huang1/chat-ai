b0d8b7189556a9658c412a74540eb3b0b8865e57
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-11 14:11:38 +0800

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { useState, useEffect } from 'react'
+
+const clerkFrontendApi = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API;
+
+function MyApp({ Component, pageProps }) {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null
+  }
+
+  return (
+    <ClerkProvider frontendApi={clerkFrontendApi}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
+}
+
+export default MyApp
