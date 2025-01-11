095942df6d7f123d814fe219cb775bf7aab4f64d
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-11 21:07:38 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,9 @@
 import { ChatInterface } from '@/components/chat-interface'
-import { ThemeToggle } from '@/components/theme-toggle'
+//import { ThemeToggle } from '@/components/theme-toggle'
 import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background flex flex-col items-center justify-center p-4">
-      <div className="w-full max-w-2xl mx-auto mb-4 flex justify-between items-center">
-        <ThemeToggle />
-      </div>
-      <SignedIn>
-        <ChatInterface />
-      </SignedIn>
-      <SignedOut>
-        <SignIn />
-      </SignedOut>
-    </div>
+    <ChatInterface />
   )
 }
