f522841e5ef288a6411e3b9ff600a5a5b7a33c1a
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-11 21:08:29 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { ChatInterface } from '@/components/chat-interface'
-//import { ThemeToggle } from '@/components/theme-toggle'
-import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs"
+// import { ThemeToggle } from '@/components/theme-toggle'
+// import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs"
 
 export default function Home() {
   return (
